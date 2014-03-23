1ca4b7082cb2904d8ed86e91e57e7f7e04bedac0
franciscog <frankjg@gmail.com>
2014-03-23 13:42:11 -0400

    adding the new files for displaying local db

---
+++ src/modules/dp.user.js
@@ -0,0 +1,27 @@
+var DrivePass = DrivePass || {};
+
+DrivePass.User = (function() {
+
+  var template = function(rowInfo) {
+    var row = document.querySelector('#row_template');
+    var rowClone = document.importNode(row.content, true);
+    rowClone.querySelector('.site').textContent = rowInfo.site;
+    rowClone.querySelector('.username').textContent = rowInfo.username;
+    rowClone.querySelector('.password').textContent = rowInfo.password;
+    document.getElementById('db_container').appendChild(rowClone);
+  };
+
+  var renderView = function() {
+    _.each(DrivePass.DB().get(), function(el, i, list) {
+      template(list[i]);
+    });
+  };
+
+  var init = function() {
+    renderView();
+  };
+
+  return {
+    init: init
+  };
+});
